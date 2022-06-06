171d60de8d093b28b79045b8a21decc905681fc5
Priyam-Pramanik <86661996+Priyam-Pramanik@users.noreply.github.com>
2022-06-06 13:30:23 +0200

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1i6DmqIjvGGqBmlBC3W0J4dXBuP3C-DQ",
+    authDomain: "letschatapp-db.firebaseapp.com",
+    databaseURL: "https://letschatapp-db-default-rtdb.firebaseio.com",
+    projectId: "letschatapp-db",
+    storageBucket: "letschatapp-db.appspot.com",
+    messagingSenderId: "774658590990",
+    appId: "1:774658590990:web:a22cce309a70b1cb546f5f"
+  };
+  
+ 
+  const app = initializeApp(firebaseConfig);
